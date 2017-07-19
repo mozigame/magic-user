142822395bcf53a3359cfdc66f90080af97ff7c9
zhongjie <zhongjie@17shihui.com>
2017-07-20 02:03:42 +0800

    调整验证码字体大小

--- magic-user-service/src/main/java/com/magic/user/util/CodeImageUtil.java
+++ magic-user-service/src/main/java/com/magic/user/util/CodeImageUtil.java
@@ -120,8 +120,8 @@ public class CodeImageUtil {
         //shear(g2, w, h, c);// 使图片扭曲
 
         g2.setColor(new Color(255,0,0));
-        int fontSize = h-8;
-        Font font = new Font("Times New Roman", Font.BOLD, fontSize);
+        int fontSize = h-10;
+        Font font = new Font("Algerian", Font.BOLD, fontSize);
         g2.setFont(font);
         char[] chars = code.toCharArray();
         for(int i = 0; i < verifySize; i++){
