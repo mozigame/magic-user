db3f67d16843549dd0250796191b779c5f1765c6
zhongjie <zhongjie@17shihui.com>
2017-07-20 01:34:20 +0800

    验证码调整

--- magic-user-service/src/main/java/com/magic/user/util/CodeImageUtil.java
+++ magic-user-service/src/main/java/com/magic/user/util/CodeImageUtil.java
@@ -125,9 +125,9 @@ public class CodeImageUtil {
         g2.setFont(font);
         char[] chars = code.toCharArray();
         for(int i = 0; i < verifySize; i++){
-            AffineTransform affine = new AffineTransform();
-            affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (w / verifySize) * i + fontSize/2, h/2);
-            g2.setTransform(affine);
+            //AffineTransform affine = new AffineTransform();
+            //affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (w / verifySize) * i + fontSize/2, h/2);
+            //g2.setTransform(affine);
             g2.drawChars(chars, i, 1, ((w-10) / verifySize) * i + 5, h/2 + fontSize/2 - 10);
         }
 
