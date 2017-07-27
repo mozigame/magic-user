24ed3e2f7470d11ce097442c6e0fe4893e6f2a36
justin <justins1707@gmail.com>
2017-07-27 09:32:33 +0800

    test

--- magic-user-service/src/test/java/com/magic/user/service/MemberServiceTest.java
+++ magic-user-service/src/test/java/com/magic/user/service/MemberServiceTest.java
@@ -1,17 +1,14 @@
 package com.magic.user.service;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * User: joey
  * Date: 2017/5/6
  * Time: 18:29
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring-application.xml"})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:spring-application.xml"})
 public class MemberServiceTest {
 	@Test
 	public void test(){
